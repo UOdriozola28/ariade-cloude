f985b372856e6c0336feffe7bc3ecda09a35863f
Walinn Paredes <paolitomaldonadofarje@gmail.com>
2026-03-08 19:43:48 -0500

    Add interface ApiResponseSearch

--- client/src/types/apiResponse.ts
+++ client/src/types/apiResponse.ts
@@ -1,4 +1,10 @@
+import type { DataSearchs } from "./results";
+
 export interface ApiResponseSimple {
   success: boolean,
   message: string,
+}
+
+export interface ApiResponseSearch extends ApiResponseSimple {
+  results: DataSearchs
 }
